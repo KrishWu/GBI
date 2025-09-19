Fix fitting numerical issues and update mass variables to time variables
Co-authored-by: KrishWu <66881117+KrishWu@users.noreply.github.com>

src/plotting/plotting.py
@@ -100,8 +100,8 @@ def plot_mu_scan_results(
     }
 
     # -------------------- making plots --------------------
-    mx = metadata["mx"]
-    my = metadata["my"]
+    tx = metadata["tx"]
+    ty = metadata["ty"]
     num_B = metadata["num_B"]
     use_full_stats = metadata["use_full_stats"]
     use_perfect_bkg_model = metadata["use_perfect_modelB"]
@@ -116,7 +116,7 @@ def plot_mu_scan_results(
     if use_bkg_model_gen_data:
         text += ", use model B to generate bkgs in data"
 
-    text += f"//Signal at $(m_X, m_Y) = ({mx}, {my})$ GeV"
+    text += f"//Signal at $(t_X, t_Y) = ({tx}, {ty})$ ms"
 
     xticks = [1e-4, 2e-4, 5e-4, 1e-3, 2e-3, 5e-3, 1e-2, 5e-2]
     xticklabels = ["0.01", "0.02", "0.05", "0.1", "0.2", "0.5", "1", "5"]
@@ -222,8 +222,8 @@ def plot_mu_scan_results_multimodels(
     }
 
     # -------------------- making plots --------------------
-    mx = metadata["mx"]
-    my = metadata["my"]
+    tx = metadata["tx"]
+    ty = metadata["ty"]
     num_B = metadata["num_B"]
     use_full_stats = metadata["use_full_stats"]
     num_ensemble = metadata["num_ensemble"]
@@ -234,7 +234,7 @@ def plot_mu_scan_results_multimodels(
     # else:
     #     text = f"Lumi matched, {num_ensemble} ensembles"
 
-    # text += f"//Signal at $(m_X, m_Y) = ({mx}, {my})$ GeV"
+    # text += f"//Signal at $(t_X, t_Y) = ({tx}, {ty})$ ms"
 
     xticks = [1e-4, 2e-4, 5e-4, 1e-3, 2e-3, 5e-3, 1e-2, 5e-2]
     xticklabels = ["0.01", "0.02", "0.05", "0.1", "0.2", "0.5", "1", "5"]
@@ -303,15 +303,15 @@ def plot_mu_scan_results_multimodels(
 
 def plot_event_feature_distribution(dfs, misc, plot_options, output_path):
 
-    mx = misc["mx"]
-    my = misc["my"]
+    tx = misc["tx"]
+    ty = misc["ty"]
     numB = misc["numB"]
     use_full_stats = misc["use_full_stats"]
     use_perfect_modelB = misc["use_perfect_modelB"]
     use_modelB_genData = misc["use_modelB_genData"]
     columns = misc["columns"]
 
-    # text = f"Signal at ({mx}, {my}) GeV \nmu_true = {mu_true}%, significance = {sig_significance}, mu_test = {mu_test}%"
+    # text = f"Signal at ({tx}, {ty}) ms \nmu_true = {mu_true}%, significance = {sig_significance}, mu_test = {mu_test}%"
     # if use_full_stats:
     #     text += "\n full stats"
     # else:
@@ -454,48 +454,3 @@ def plot_mu_scan_results_multimass(
     plt.close()
 
 
-def plot_event_feature_distribution(dfs, misc, plot_options, output_path):
-
-    mx = misc["mx"]
-    my = misc["my"]
-    numB = misc["numB"]
-    use_full_stats = misc["use_full_stats"]
-    use_perfect_modelB = misc["use_perfect_modelB"]
-    use_modelB_genData = misc["use_modelB_genData"]
-    columns = misc["columns"]
-
-    # text = f"Signal at ({mx}, {my}) GeV \nmu_true = {mu_true}%, significance = {sig_significance}, mu_test = {mu_test}%"
-    # if use_full_stats:
-    #     text += "\n full stats"
-    # else:
-    #     text += "\n lumi matched"
-
-    # if use_perfect_modelB:
-    #     text += ", model B trained in SR"
-    # if use_modelB_genData:
-    #     text += ", use model B to generate bkgs in data"
-    # if not use_modelB_genData and not use_perfect_modelB:
-    #     text += ", model B trained in CR"
-
-    plotter = VariableDistributionPlot(dfs, plot_options=plot_options)
-    # plotter.add_text(text, 0.05, 0.95, fontsize=18)
-
-    with PdfPages(output_path) as pdf:
-        for feature in columns:
-            plotter.draw(
-                feature,
-                logy=False,
-                bins=np.linspace(
-                    dfs["Background"][feature].min(),
-                    dfs["Background"][feature].max(),
-                    101,
-                ),
-                unit="GeV",
-                show_error=False,
-                comparison_options=None,
-                xlabel=feature,
-            )
-            # axis.set_title(f"{feature} distribution", fontsize=18)
-            # axis.set_ylim(0, 0.25)
-            pdf.savefig(bbox_inches="tight")
-            plt.close()

src/tasks/bkgsampling.py
@@ -45,10 +45,10 @@ class SampleModelBinSR(
     def run(self):
         # load SR real bkgs
         SR_bkg_data = np.load(self.input()["bkg_data"]["SR_bkg"].path)
-        # load mass in the first column
-        mass_bkg_data = SR_bkg_data[:, 0]
-        mass_bkg_data = (
-            torch.from_numpy(mass_bkg_data)
+        # load time in the first column
+        time_bkg_data = SR_bkg_data[:, 0]
+        time_bkg_data = (
+            torch.from_numpy(time_bkg_data)
             .reshape((-1, 1))
             .type(torch.FloatTensor)
             .to(self.device)
@@ -73,14 +73,14 @@ class SampleModelBinSR(
         with torch.no_grad():
             sampled_SR_events = model_B.model.sample(
                 num_samples=SR_bkg_data.shape[0],
-                cond_inputs=mass_bkg_data,
+                cond_inputs=time_bkg_data,
             )
 
         sampled_SR_events = sampled_SR_events.cpu().numpy()
 
-        # add mass column
+        # add time column
         sampled_SR_events = np.concatenate(
-            (mass_bkg_data.cpu().numpy(), sampled_SR_events), axis=1
+            (time_bkg_data.cpu().numpy(), sampled_SR_events), axis=1
         )
         # add last column of 0 for bkg
         sampled_SR_events = np.concatenate(
@@ -124,7 +124,7 @@ class PreprocessingFoldwModelBGen(
                 "data_SR_data_trainval_model_B.npy"
             ),
             "SR_data_test_model_B": self.local_target("data_SR_data_test_model_B.npy"),
-            "SR_mass_hist": self.local_target("SR_mass_hist.json"),
+            "SR_time_hist": self.local_target("SR_time_hist.json"),
         }
 
     @law.decorator.safe_output
@@ -148,15 +148,15 @@ class PreprocessingFoldwModelBGen(
         for key in pre_parameters.keys():
             pre_parameters[key] = np.array(pre_parameters[key])
 
-        # ----------------------- mass hist in SR -----------------------
+        # ----------------------- time hist in SR -----------------------
         from config.configs import SR_MIN, SR_MAX
 
-        mass = SR_bkg[SR_bkg[:, -1] == 0, 0]
+        time = SR_bkg[SR_bkg[:, -1] == 0, 0]
         bins = np.linspace(SR_MIN, SR_MAX, 50)
-        hist_back = np.histogram(mass, bins=bins, density=True)
-        # save mass histogram and bins
-        self.output()["SR_mass_hist"].parent.touch()
-        with open(self.output()["SR_mass_hist"].path, "w") as f:
+        hist_back = np.histogram(time, bins=bins, density=True)
+        # save time histogram and bins
+        self.output()["SR_time_hist"].parent.touch()
+        with open(self.output()["SR_time_hist"].path, "w") as f:
             json.dump({"hist": hist_back[0], "bins": hist_back[1]}, f, cls=NumpyEncoder)
 
         # ----------------------- make SR data -----------------------